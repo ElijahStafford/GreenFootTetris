f0d16902e7f482fceca56fb21eca6628d7fd4017
ElijahStafford <124601746+ElijahStafford@users.noreply.github.com>
2023-04-15 19:35:39 -0400

    vector math

--- GreenFootTetris/Vector2.java
+++ GreenFootTetris/Vector2.java
@@ -1,13 +1,36 @@
 public class Vector2 {
-    public int x = 0;
-    public int y = 0;
+    public double x = 0;
+    public double y = 0;
 
-    public Vector2(int x, int y) {
+    public Vector2(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    public Vector2(int x) {
+    public Vector2(double x) {
         this.x = x;
+    }
+
+    public void rotate(double degrees) {
+        rotate(degrees, 0, 0);
+    }
+
+    public void rotate(double degrees, double anchorX, double anchorY) {
+        Vector2 delta = new Vector2(x - anchorX, y - anchorY);
+
+        double angle = Math.atan2(-delta.y, delta.x);
+        angle += Math.toRadians(degrees);
+
+        double newX = Math.cos(angle);
+        double newY = Math.sin(angle);
+        newX *= delta.magnitude();
+        newY *= delta.magnitude();
+
+        x = newX + anchorX;
+        y = newY + anchorY;
+    }
+
+    public double magnitude() {
+        return Math.sqrt(x * x + y * y);
     }
 }
